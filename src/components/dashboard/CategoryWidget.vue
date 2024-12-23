570ee6aefd1656af163b1b557bd7e1b5864962d4
khav <khav.saroeun@gmail.com>
2024-12-23 11:10:42 +0700

    first commit

---
+++ src/components/dashboard/CategoryWidget.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card flex flex-col gap-4">
+        <div class="flex flex-wrap gap-2">
+            <Button label="Coffee" rounded/>
+            <Button label="Non Coffee" rounded outlined/>
+            <Button label="Food" rounded outlined/>
+            <Button label="Snack" rounded outlined/>
+            <Button label="Dessert" rounded outlined/>
+        </div>
+    </div>
+</template>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CategoryWidget from '@/components/dashboard/CategoryWidget.vue';
+import ListProductWidget from '@/components/dashboard/ListProductWidget.vue';
+</script>
+
+<template>
+
+    <CategoryWidget/>
+    <ListProductWidget/>
+
+</template>
